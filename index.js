9df7038af253be12ec9148a0bd11d1957aee661f
sutapaaditya <sutapaaditya84@gmail.com>
2023-02-16 17:16:37 +0530

    New_photo_and_data_added

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 let users = [
   {
-    "name" : "Jasmin P.",
+    "name" : "chandrima saha.",
     "designation": "Assistant Teacher",
-    "pic": "./pictures/pic1.jpg",
+    "pic": "./pictures/p3.jpg",
     "testimonial": "Lorem ipsum dolor sit amet, officia excepteur ex fugiat reprehenderit enim labore culpa sint ad nisi Lorem pariatur mollit ex esse exercitation amet. Nisi anim cupidatat excepteur officia."
   },
   {
